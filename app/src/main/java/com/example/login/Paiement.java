63bd4bbcbe7f3cdd188f36a0e3bd7a0134982086
AzioizAnass <azioiz.anass@gmail.com>
2021-12-18 13:01:53 +0100

    paiement activity

--- app/src/main/java/com/example/login/Paiement.java
+++ app/src/main/java/com/example/login/Paiement.java
@@ -1,14 +1,64 @@
 package com.example.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Paiement extends AppCompatActivity {
-
+TextView commande ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paiement);
+
+        Button annulerBtn , confirmerBtn ;
+
+        commande= findViewById(R.id.tvCommande);
+        annulerBtn= findViewById(R.id.annulerBtn);
+        confirmerBtn = findViewById(R.id.confirmerBtn);
+
+        String cmdID = getIntent().getStringExtra("plat_name") ;
+        String idRestau = getIntent().getStringExtra("restaurant_id");
+        //commande.setText(cmdID);
+        //.child(idRestau)
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Restaurants").child(idRestau);
+        ref.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String nomRestau = snapshot.child("Nom").getValue().toString();
+                String nomPlat = snapshot.child("Menu").child(cmdID).child("NomPlat").getValue().toString();
+
+                commande.setText(nomPlat +" chez "+ nomRestau);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        annulerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Paiement.this , PlatModel.class);
+                startActivity(i);
+            }
+        });
+        //confirmerBtn.setOnClickListener();
     }
 }
--- app/src/main/java/com/example/login/PlatProfil.java
+++ app/src/main/java/com/example/login/PlatProfil.java
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +23,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class PlatProfil extends AppCompatActivity {
 TextView textView1,textView2,textView3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +33,8 @@ TextView textView1,textView2,textView3;
         int PlatId = getIntent().getIntExtra("platId", 666);
         String platId = String.valueOf(PlatId);
         //String restauId = String.valueOf(RestauId) ;
-
+        Button commander ;
+        commander = (Button)findViewById(R.id.commanderBtn);
         DatabaseReference database;
 
         textView1 = findViewById(R.id.platNom);
@@ -54,6 +59,17 @@ TextView textView1,textView2,textView3;
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+
+
+        commander.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PlatProfil.this, Paiement.class);
+                i.putExtra("plat_name", platId) ;
+                i.putExtra("restaurant_id",restauId);
+                startActivity(i);
             }
         });
 
